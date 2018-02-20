45c2d80de0d6b71ca0118f752e482a4e34f56a1a
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-02-20 14:11:22 +0000

    Added the table and columns of the menu items.

--- database/database_setup.py
+++ database/database_setup.py
@@ -29,7 +29,21 @@ class Restaurant(Base):
                 Integer, primary_key = True)
 
 # Menu table
-
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+    name = Column(
+                  String(80), nullable = False)
+    id = Column(
+                Integer, priamry_key = True)
+    course = Column(
+                    String(80))
+    description = Column(
+                         String(250))
+    price = Column(
+                   String(8))
+    restaurant_id = Column(
+                           Integer, ForeignKey = 'restaurant.id')
+    restaurant = relationship(Restaurant)
 
 ############
 # Create the database
