d52f319f04ec609feaf5d1f2ec57a669759ce046
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-05 11:22:28 +0530

    Added the response and added the outputto the output stream

--- database/webserver.py
+++ database/webserver.py
@@ -2,6 +2,15 @@ from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 class webserverHandler(self):
 	try:
+		if self.path.endswith("/hello"):
+			self.send_response(200)
+			self.send_header('Content type', 'text/html')
+			self.end_headers()
+
+			output = ""
+			output += "<html><body>Hello!</body></html>"
+			self.wfile.write(output)
+			print output
 
 	except IOError:
 		self.send_error(404, "Requested file not found at %s" % self.path)
