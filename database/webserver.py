1ef8eeb93454dc10bf0852774b0753aaabcf7681
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-05 07:49:47 +0530

    Wrote the main function

--- database/webserver.py
+++ database/webserver.py
@@ -1,1 +1,18 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, BaseHTTPServer
+
+def main():
+	try:
+		port = 8080
+		server = HTTPserver(('',port), webserverHandler)
+		print "Web server is running on port %s" % port
+		serve.serve_forever()
+	except KeyboardInterrupt:
+		print("^C entered, stopping server...")
+		server.socket.close
+	else:
+		pass
+	finally:
+		pass
+
+if __name__ == '__main__':
+	main()
