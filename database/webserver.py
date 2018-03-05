f20dd8e47c40ae5c0b833e2aff76595dc2861cf7
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-05 12:28:49 +0530

    Main

--- database/webserver.py
+++ database/webserver.py
@@ -2,9 +2,14 @@ from BaseHttpServer import HTTPServer, BaseHTTPRequestHandler
 
 def main():
 	try:
-		
+		port = 8080
+		server = HTTPServer(('', port), webserverHandler)
+		print "Server is running on port %s" % port
+		server.run_forever()
 
 	except KeyboardInterrupt:
+		print " ^C entered, closing server..."
+		server.socket.close()
 
 if __name__ == '__main__':
 	main()
