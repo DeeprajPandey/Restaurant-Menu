1c622febb412259c2b8d2a2d990d20968d2f4aa3
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-05 11:30:30 +0530

    do_get function instead of contructor in handler class

--- database/webserver.py
+++ database/webserver.py
@@ -1,20 +1,21 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
-class webserverHandler(self):
-	try:
-		if self.path.endswith("/hello"):
-			self.send_response(200)
-			self.send_header('Content type', 'text/html')
-			self.end_headers()
+class webserverHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if self.path.endswith("/hello"):
+				self.send_response(200)
+				self.send_header('Content type', 'text/html')
+				self.end_headers()
 
-			output = ""
-			output += "<html><body>Hello!</body></html>"
-			self.wfile.write(output)
-			print output
-			return
+				output = ""
+				output += "<html><body>Hello!</body></html>"
+				self.wfile.write(output)
+				print output
+				return
 
-	except IOError:
-		self.send_error(404, "Requested file not found at %s" % self.path)
+		except IOError:
+			self.send_error(404, "Requested file not found at %s" % self.path)
 
 def main():
 	try:
