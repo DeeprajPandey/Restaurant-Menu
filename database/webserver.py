e64b98438fefda4b01f29ffd8709a7eb7f030172
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-05 12:34:44 +0530

    Server script complete

--- database/webserver.py
+++ database/webserver.py
@@ -1,11 +1,28 @@
-from BaseHttpServer import HTTPServer, BaseHTTPRequestHandler
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+def webserverHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if self.path.endswith("/hello"):
+				self.send_response(200)
+				self.send_header('Content type', 'text/html')
+				self.end_headers()
+
+				output = ""
+				output += "<html><body>Hello!</body></html>"
+				self.wfile.write(output)
+				print output
+				return
+		except IOError:
+			self.send_error(404, "File not found in %s" % self.path)
+
 
 def main():
 	try:
 		port = 8080
 		server = HTTPServer(('', port), webserverHandler)
 		print "Server is running on port %s" % port
-		server.run_forever()
+		server.serve_forever()
 
 	except KeyboardInterrupt:
 		print " ^C entered, closing server..."
