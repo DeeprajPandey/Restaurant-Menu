fb8545775ed87187078a4cbf135c911bb71c0298
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-05 11:16:36 +0530

    Wrote the main function

--- database/webserver.py
+++ database/webserver.py
@@ -1,35 +1,14 @@
-import BaseHTTPServer
-from BaseHTTPServer import BaseHTTPRequestHandler, BaseHTTPServer
-
-class webserverHander(BaseHTTPRequestHandler):
-	def do_GET(self):
-		try:
-			if self.path.endswith("/hello"):
-				self.send_response(200)
-				self.send_header('Content type', 'text/html')
-				self.end_headers()
-
-				output=""
-				output+="<html><body>Hello!</body></html>"
-				self.wfile.write(output)
-				print output
-				return
-		except IOError:
-			self.send_error(404, "File not found %s" % self.path)
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 def main():
 	try:
 		port = 8080
-		server = HTTPserver(('',port), webserverHandler)
-		print "Web server is running on port %s" % port
-		serve.serve_forever()
+		server = HTTPServer(('', port), webserverHandler)
+		print "Server is running on port %s" % port
+		server.serve_forever()
 	except KeyboardInterrupt:
-		print("^C entered, stopping server...")
-		server.socket.close
-	else:
-		pass
-	finally:
-		pass
+		print " ^C entered, stopping server..."
+		server.socket.close()
 
 if __name__ == '__main__':
 	main()
