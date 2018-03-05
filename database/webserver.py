0c2d1a63337791f75b77c45bc27e2cadd1bdfaa8
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-05 13:11:26 +0530

    Basic setup for POST

--- database/webserver.py
+++ database/webserver.py
@@ -1,5 +1,6 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-
+# Common Get Interface for POST
+import cgi
 class webserverHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		try:
@@ -13,14 +14,32 @@ class webserverHandler(BaseHTTPRequestHandler):
 				self.wfile.write(output)
 				print output
 				return
+			if self.path.endswith("/hola"):
+				self.send_response(200)
+				self.send_header('Content type', 'text/html')
+				self.end_headers()
+
+				output=""
+				output+="<html><body>&#161Hola!<a href='/hello'>Back to hello</a></body></html>"
+				self.wfile.write(output)
+				print output
+				return
 		except IOError:
 			self.send_error(404, "File not found at %s" % self.path)
+	def do_POST(self):
+		try:
+			self.send_response(301)
+			self.end_headers()
+
+			
+
+		except:
 
 def main():
 	try:
 		port = 8080
-		server = HTTPServer(('127.0.80.90', port), webserverHandler)
-		print "Server running at 127.0.80.90:%s" % port
+		server = HTTPServer(('', port), webserverHandler)
+		print "Server running on port %s" % port
 		server.serve_forever()
 	except KeyboardInterrupt:
 		print " ^C entered, stopping webserver..."
