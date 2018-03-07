228eeed6940b4311d649ec638b19e94acfb8bc32
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-07 21:14:39 +0530

    Added GET response to /restaurants

--- database/webserver.py
+++ database/webserver.py
@@ -32,18 +32,10 @@ class webServerHandler(BaseHTTPRequestHandler):
                 print output
                 return
 
-            if self.path.endswith("/hola"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = ""
-                output += "<html><body>"
-                output += "<h1>&#161 Hola !</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
-                return
+            if self.path.endswith("/restaurants"):
+            	self.send_response(200)
+            	self.send_header('Content-type', 'text/html')
+            	self.end_headers()
 
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
