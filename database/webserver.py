02e941b334f97f44cea99824c6455a033f987140
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-07 21:06:33 +0530

    Import SQLAlchemy and ORM to add CRUD functionality to server

--- database/webserver.py
+++ database/webserver.py
@@ -1,6 +1,8 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
 class webServerHandler(BaseHTTPRequestHandler):
 
