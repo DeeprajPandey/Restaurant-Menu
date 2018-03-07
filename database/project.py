3cb4e61129c8703e717fef2b5e514a85cb9f3d3a
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-08 02:50:16 +0530

    SQLite for CRUB.

--- database/project.py
+++ database/project.py
@@ -1,7 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
-# @app.route('/')
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+# Bind the engine to metadata of Base so declaratives can be accessed through DBSession
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
 @app.route('/hello')
 def HelloWorld():
 	return "Hey there!"
