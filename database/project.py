9cbd430dc236bb379c684f0bd4db92c0685da2bb
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-08 15:53:24 +0530

    Return therendered template from the flask file instead of a string of html code

--- database/project.py
+++ database/project.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -16,16 +16,8 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
 	items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-	output = restaurant.name + '<br/>'
-	for i in items:
-		output += i.name
-		output += '</br>'
-		output += i.price
-		output += '</br>'
-		output += i.description
-		output += '</br>'
-		output += '</br>'
-	return output
+	return render_template('menu.html', restaurant=restaurant, items=items)
+
 @app.route('/restaurants/<int:restaurant_id>/new')
 def newMenuItem(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
