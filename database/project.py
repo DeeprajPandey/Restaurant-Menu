72c9fdb10ca7443b5f7b0bbbc1b0d23be9d7d1ae
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-08 09:24:18 +0530

    Added the routing function for editing a menu item

--- database/project.py
+++ database/project.py
@@ -30,8 +30,10 @@ def restaurantMenu(restaurant_id):
 def newMenuItem(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
 
-
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+	item = session.query(MenuItem).filter_by(id=menu_id).one()
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
 
