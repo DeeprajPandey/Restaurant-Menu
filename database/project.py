3735b0b84a086c79428fdf1dd7cabb96a9184357
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-03-08 09:13:51 +0530

    Routing address for create, edit and delete menu items

--- database/project.py
+++ database/project.py
@@ -14,19 +14,24 @@ session = DBSession()
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = restaurant.name + '<br/>'
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
-    return output
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+	output = restaurant.name + '<br/>'
+	for i in items:
+		output += i.name
+		output += '</br>'
+		output += i.price
+		output += '</br>'
+		output += i.description
+		output += '</br>'
+		output += '</br>'
+	return output
+@app.route('/restaurants/<int:restaurant_id>/new')
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
